8dea21802b4cf6f7221a965845fd6944e259ed99
ApurvaP04 <59483865+Apurva-04@users.noreply.github.com>
2021-08-21 02:35:01 +0530

    4

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -13,31 +13,9 @@ class HomePage extends Component {
 					<Header onLogin={(route) => this.props.history.push(route)} />
 					<AboutUs />
 				</div>
-				<Footer className="footer" />
 			</div>
 		)
 	}
 }
 
 export default HomePage;
-
-/*
-<div className="content" id="homepage-flex">
-<SimpleCard
-	image={plagiarism}
-	title='Plagiarism detection'
-	body='It instantly detects if a student has copied from internet or from other students. It clusterizes the copies looking similar ' />
-<SimpleCard
-	image={organise}
-	title='Stay Organised'
-	body='It has separate portals for students and professors. Students can have their track of their marks and assignments.' />
-<SimpleCard
-	image={assignment}
-	title='Assignment'
-	body='It allows professors to assign tasks to students and students can submit their answer sheets using EasyMonitor.' />
-<SimpleCard
-	image={performance}
-	title='Performance tracker'
-	body='It shows your subject-wise marks of each subject. You can have a track of your progress by looking at the Bar Graphs' />
-</div>
-*/
